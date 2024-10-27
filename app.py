c116d9db96ad8da335c7de7265fc5a8a25f88086
Purna Chandra Mansingh <purnachandramansingh135@gmail.com>
2024-10-27 19:23:56 +0530

    install gunicon

--- app.py
+++ app.py
@@ -107,4 +107,4 @@ def serve_output(filename):
     return send_from_directory(app.config['OUTPUT_FOLDER'], filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
