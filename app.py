cb14b3cfdbe7872d0e7b023f54b334912378fce9
Purna Chandra Mansingh <purnachandramansingh135@gmail.com>
2024-10-27 20:11:29 +0530

    update for python anywhere

--- app.py
+++ app.py
@@ -107,4 +107,4 @@ def serve_output(filename):
     return send_from_directory(app.config['OUTPUT_FOLDER'], filename)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run()
